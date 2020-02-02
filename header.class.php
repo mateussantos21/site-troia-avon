29a03e3fae3aefa2d222f79cb68b878df86a9c59
Mateus Santos <santos.mrodrigues0@gmail.com>
2020-02-02 13:15:41 -0300

    processo seletivo button on header

--- header.class.php
+++ header.class.php
@@ -103,6 +103,11 @@
 						</div>
 						<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 							<ul class="nav navbar-nav">
+								<li class="processo">
+									<a href="" class="smoothScroll" class="btn btn-outline-success" type="button">
+										<?php if($idioma == 'true'){echo "Processo Seletivo";}else{echo "";}?>
+									</a>
+								</li>
 								<li>
 									<a href="
 										<?php $endereco = $_SERVER ['REQUEST_URI'];
